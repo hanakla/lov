3d1eec9144fd1454999bf55195c9f07cbf4c2b69
Ragg <Ragg-@users.noreply.github.com>
2016-08-13 20:23:10 +0900

    Add exclusion status id

--- app/entry.js
+++ app/entry.js
@@ -30,6 +30,9 @@ const BLACKLISTED_STATUS_IDS = [
     // Pasted retweets
     "744762639180570624",
 
+    // Unrelated tweets
+    "764266522940354561",
+
     // INM
     "748084432855138304",
     "748073477488381956",
