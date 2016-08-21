084f1b894b57a81e177da2a5725c4761ff8230c1
Ragg <Ragg-@users.noreply.github.com>
2016-08-21 17:45:38 +0900

    remove comment

--- app/exclusions.js
+++ app/exclusions.js
@@ -12,8 +12,6 @@ module.exports = {
         // Unrelated tweets
         "764266522940354561",
         "766441366838648833",
-
-        // INM
         "748084432855138304",
         "748073477488381956",
     ].map(statusId => Long.fromString(statusId))
